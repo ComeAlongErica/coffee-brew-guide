b6ba75bd14252c65a17e6132901ac669129139af
ComeAlongErica <ericaraephoto@gmail.com>
2019-03-17 10:58:58 -0400

    Installed styled-components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import styled from 'styled-styled-components'
+
 import Search from './components/Search'
 import User from './components/User'
 import Save from './components/Save'
 import Recipes from './components/Recipes'
 import Blog from './components/Blog'
+
 
 class App extends Component {
   render() {
