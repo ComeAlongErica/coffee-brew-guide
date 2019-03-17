1e3deb48bdb218d1b560d063733314b3d9ecf3ff
ComeAlongErica <ericaraephoto@gmail.com>
2019-03-17 10:52:51 -0400

    import all components and display is working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Search from './components/Search'
+import Save from './components/Save'
+import Recipes from './components/Recipes'
+import Blog from './components/Blog'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Search />
+      <Save />
+      <Recipes />
+      <Blog />
       </div>
     );
   }
