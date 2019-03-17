28c05bd972c5d85081faed441613a6eb5b4460a4
ComeAlongErica <ericaraephoto@gmail.com>
2019-03-17 11:01:06 -0400

    Added container folder for clean up

--- src/container/App.js
+++ src/container/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import styled from 'styled-styled-components'
+import styled from 'styled-components'
 
-import Search from './components/Search'
-import User from './components/User'
-import Save from './components/Save'
-import Recipes from './components/Recipes'
-import Blog from './components/Blog'
+import Search from '../components/Search'
+import User from '../components/User'
+import Save from '../components/Save'
+import Recipes from '../components/Recipes'
+import Blog from '../components/Blog'
 
 
 class App extends Component {
