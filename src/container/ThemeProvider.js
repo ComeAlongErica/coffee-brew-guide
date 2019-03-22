e803da31f66a3bf42a693db19823b2b9d1d365ca
ComeAlongErica <ericaraephoto@gmail.com>
2019-03-22 17:41:33 -0400

    begin light and dark theme

---
+++ src/container/ThemeProvider.js
@@ -0,0 +1,22 @@
+const lightTheme = {
+  fontMain: 'grey',
+  fontSecondary: 'dark-grey',
+  fontAccent: 'light-grey',
+  background: 'white',
+  backgroundSeconary: 'black',
+  accent: 'yellow',
+  dropShadow: '',
+}
+
+const darkTheme = {
+  fontMain: 'white',
+  fontSecondary: 'light-grey',
+  fontAccent: 'dark-grey',
+  background: 'black',
+  backgroundSeconary: 'white',
+  accent: 'yellow',
+  dropShadow: '',
+}
+
+export default lightTheme
+export default darkTheme
