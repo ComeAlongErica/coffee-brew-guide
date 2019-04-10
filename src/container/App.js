3f7cdc4be6f57942cb2b7818430c93ea8ffacda4
ComeAlongErica <ericaraephoto@gmail.com>
2019-04-09 21:39:41 -0400

    added fetch for api call

--- src/container/App.js
+++ src/container/App.js
@@ -14,26 +14,44 @@ background-color: ${props => props.theme.background};
 
 
 class App extends Component {
-  state = {
-    theme: true
+  constructor(props) {
+    super(props)
+    this.state = {
+      theme: true,
+      modal: [],
+    }
+  }
+
+  getRecipeData = (query) => {
+    // let url = `https://api.edamam.com/search?q=${query}`
+    // for quick testing in localhost 
+    //open -a Google\ Chrome --args --disable-web-security --user-data-dir
+    let url = 'https://api.edamam.com/search?q=tofu&app_id=c0958c7a&app_key=b4e42092e83e921feb2a01415d4496f5'
+      console.log(url)
+    fetch(url).then(res => res.json())
+      .then((result) => {
+        console.log(result)
+      })
+      .catch(error => console.error(error))
   }
 
   handleToggleTheme = () => {
-    this.setState({ theme: !this.state.theme})
+    this.setState({ theme: !this.state.theme })
   }
 
   render() {
+    this.getRecipeData()
     let displayTheme = this.state.theme ? lightTheme : darkTheme
     return (
       <ThemeProvider theme={displayTheme}>
         <AppContainer className={'appContainer'}>
-          <Header toggleTheme={this.handleToggleTheme}/>
+          <Header toggleTheme={this.handleToggleTheme} />
           {/* <Recipes />
           <Blog /> */}
         </AppContainer>
       </ThemeProvider>
 
-    );
+    )
   }
 }
 
