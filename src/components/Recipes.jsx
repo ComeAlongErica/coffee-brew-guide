6c9bf4536886478ce4b502cd87c3b8433006647d
ComeAlongErica <ericaraephoto@gmail.com>
2019-04-23 17:48:02 -0400

    displaying recipes and images in recipe section

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect, Fragment } from 'react'
 
-const Recipes = () => {
+const Recipes = props => {
+  const { getRecipeData } = props
+  const [recipes, setRecipes] = useState()
+
+  useEffect(() => {
+    getRecipeData('espresso').then(data => setRecipes(data.hits))
+  }, [])
+
+  console.log(recipes)
 
   return (
-    <div>
-      This is Recipes Component
-    </div>
-  );
+    <Fragment>
+      {recipes && recipes.map((recipe, idx) => {
+        return <div key={idx}>
+          <h3>{recipe.recipe.label}</h3>
+          <img src={recipe.recipe.image} />
+          <p>{recipe.recipe.ingredientLines}</p>
+          <p>Calories: {recipe.recipe.calories / recipe.recipe.yield}</p>
+        </div>
+      })}
+    </Fragment>
+  )
 }
 
-export default Recipes;
+export default Recipes
--- src/container/App.js
+++ src/container/App.js
@@ -3,7 +3,7 @@ import styled, { ThemeProvider } from 'styled-components'
 
 import { lightTheme, darkTheme } from '../assets/ThemeProvider'
 import Header from '../components/header/Header'
-// import Recipes from '../components/Recipes'
+import Recipes from '../components/Recipes'
 // import Blog from '../components/Blog'
 
 const AppContainer = styled.div`
@@ -23,15 +23,13 @@ class App extends Component {
   }
 
   getRecipeData = (query) => {
-    // let url = `https://api.edamam.com/search?q=${query}`
+    let url = `https://api.edamam.com/search?q=${query}&app_id=c0958c7a&app_key=b4e42092e83e921feb2a01415d4496f5`
     // for quick testing in localhost 
     //open -a Google\ Chrome --args --disable-web-security --user-data-dir
-    let url = 'https://api.edamam.com/search?q=espresso&app_id=c0958c7a&app_key=b4e42092e83e921feb2a01415d4496f5'
-      console.log(url)
-    fetch(url).then(res => res.json())
-      .then((result) => {
-        console.log(result)
-      })
+    // let url = 'https://api.edamam.com/search?q=espresso&app_id=c0958c7a&app_key=b4e42092e83e921feb2a01415d4496f5'
+
+    return fetch(url).then(res => res.json())
+      .then(result => result)
       .catch(error => console.error(error))
   }
 
@@ -40,14 +38,13 @@ class App extends Component {
   }
 
   render() {
-    this.getRecipeData()
     let displayTheme = this.state.theme ? lightTheme : darkTheme
     return (
       <ThemeProvider theme={displayTheme}>
         <AppContainer className={'appContainer'}>
           <Header toggleTheme={this.handleToggleTheme} />
-          {/* <Recipes />
-          <Blog /> */}
+          <Recipes getRecipeData={this.getRecipeData}/>
+          {/* <Blog /> */}
         </AppContainer>
       </ThemeProvider>
 
